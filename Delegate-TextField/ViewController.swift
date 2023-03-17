5e1b9c2090506331b847d32d3112c4c385e86063
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-03-17 21:43:57 +0900

    Initial Commit

---
+++ Delegate-TextField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate-TextField
+//
+//  Created by 김정우 on 2023/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Delegate-TextFieldUITests/Delegate_TextFieldUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Delegate_TextFieldUITestsLaunchTests.swift
+//  Delegate-TextFieldUITests
+//
+//  Created by 김정우 on 2023/03/17.
+//
+
+import XCTest
+
+final class Delegate_TextFieldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
